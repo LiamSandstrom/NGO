4f9ef693f0d685e9294d7706a9bab9834a77dff7
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-05-13 12:31:44 +0200

    update ui with flatLaf and login screen

--- src/NGO/Start.java
+++ src/NGO/Start.java
@@ -1,5 +1,9 @@
 package NGO;
 
+import com.formdev.flatlaf.intellijthemes.FlatDarkFlatIJTheme;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import java.util.HashSet;
+import java.util.Set;
 import javax.swing.JOptionPane;
 import oru.inf.InfDB;
 import oru.inf.InfException;
@@ -11,6 +15,7 @@ import oru.inf.InfException;
 public class Start {
 
 	public static void main(String[] args) {
+		FlatMacDarkLaf.setup();
 		new MainWindow();
 	}
 }
--- src/NGO/UI/LoginWindow.java
+++ src/NGO/UI/LoginWindow.java
@@ -9,8 +9,10 @@ import com.mysql.cj.util.StringUtils;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -33,22 +35,28 @@ public class LoginWindow extends JPanel {
 
 		//Main container
 		setLayout(new GridBagLayout());
+		int textSize  = 16;
+		int inputWidth = 350;
+		int inputHeight = 40;
 
 		//Login Content
 		JPanel content = new JPanel();
-		content.setBackground(new Color(175 , 185, 245));
+		content.setBackground(new Color(40,40,40));
 		content.setLayout(new GridBagLayout());
-		content.setPreferredSize(new Dimension(450, 150));
+		content.setPreferredSize(new Dimension(450, 230));
 		GridBagConstraints gbc = new GridBagConstraints();
-
+		
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.anchor = GridBagConstraints.WEST;
-		content.add(new JLabel("Email adress"), gbc);
+		JLabel emailLabel = new JLabel("Email adress");
+		emailLabel.setFont(new Font("Arial", Font.PLAIN, textSize));
+		content.add(emailLabel, gbc);
 
 		//Login Elements
 		JTextField email = new JTextField();
-		email.setPreferredSize(new Dimension(350, 40));
+		email.setPreferredSize(new Dimension(inputWidth, inputHeight));
+		email.setMinimumSize(new Dimension(inputWidth, inputHeight));
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		gbc.anchor = GridBagConstraints.CENTER;
@@ -56,21 +64,28 @@ public class LoginWindow extends JPanel {
 
 		gbc.gridx = 0;
 		gbc.gridy = 2;
+		gbc.insets = new Insets(20,0,0,0);
 		gbc.anchor = GridBagConstraints.WEST;
-		content.add(new JLabel("Password"), gbc);
-		
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setFont(new Font("Arial", Font.PLAIN, textSize));
+		content.add(passwordLabel, gbc);
+
 		JTextField password = new JTextField();
-		password.setPreferredSize(new Dimension(350, 40));
+		password.setPreferredSize(new Dimension(inputWidth, inputHeight));
+		password.setMinimumSize(new Dimension(inputWidth, inputHeight));
 		gbc.gridx = 0;
 		gbc.gridy = 3;
+		gbc.insets = new Insets(0,0,0,0);
 		gbc.anchor = GridBagConstraints.CENTER;
 		content.add(password, gbc);
 
 		JButton btn = new JButton("Login");
-		btn.setPreferredSize(new Dimension(200, 35));
+		btn.setFont(new Font("Arial", Font.PLAIN, textSize));
+		btn.setPreferredSize(new Dimension(inputWidth, 40));
 		gbc.gridx = 0;
 		gbc.gridy = 4;
-		
+		gbc.insets = new Insets(10,0,0,0);
+
 		content.add(btn, gbc);
 
 		//Login button event
