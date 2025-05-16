3c36ec9edc09dc6611b3c44c0b926714c08ae923
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2025-05-16 12:37:51 +0200

    ändrat att man kan få ut alla projekt och kunna ändra data i alla

--- src/NGO/UI/ChangeProjectInfo.java
+++ src/NGO/UI/ChangeProjectInfo.java
@@ -6,7 +6,12 @@ package NGO.UI;
 
 import NGO.User;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import oru.inf.InfDB;
 import oru.inf.InfException;
@@ -16,83 +21,79 @@ import oru.inf.InfException;
  * @author rostykmalanchuk
  */
 public class ChangeProjectInfo extends ContentPanelStructure {
+
     private User user;
     private String id;
     private InfDB idb;
-    
-    public ChangeProjectInfo (User user, UIStructure twoPanel){
-        super(user, twoPanel); 
-        try{
+
+    public ChangeProjectInfo(User user, UIStructure twoPanel) {
+        super(user, twoPanel);
+        try {
             setBackground(Color.LIGHT_GRAY);
             id = user.getId();
             idb = user.getDb();
-            
-            JTextField projectid = new JTextField(60);
-            JTextField projectname = new JTextField(60);
-            JTextField description = new JTextField(60);
-            JTextField startdate = new JTextField(60);
-            JTextField enddate = new JTextField(60);
-            JTextField cost = new JTextField(60);
-            JTextField status = new JTextField(60);
-            JTextField priority = new JTextField(60);
-            JTextField projectmanager = new JTextField(60);
-            JTextField country = new JTextField(60);
-            
-            projectid.setText(idb.fetchSingle("select pid from projekt where projektchef = '" + id + "'"));
-            projectname.setText(idb.fetchSingle("select projektnamn from projekt where projektchef = '" + id + "'"));
-            description.setText(idb.fetchSingle("select beskrivning from projekt where projektchef = '" + id + "'"));
-            startdate.setText(idb.fetchSingle("select startdatum from projekt where projektchef = '" + id + "'"));
-            enddate.setText(idb.fetchSingle("select slutdatum from projekt where projektchef = '" + id + "'"));
-            cost.setText(idb.fetchSingle("select kostnad from projekt where projektchef = '" + id + "'"));
-            status.setText(idb.fetchSingle("select status from projekt where projektchef = '" + id + "'"));
-            priority.setText(idb.fetchSingle("select prioritet from projekt where projektchef = '" + id + "'"));
-            projectmanager.setText(idb.fetchSingle("select projektchef from projekt where projektchef = '" + id + "'"));
-            country.setText(idb.fetchSingle("select land from projekt where projektchef = '" + id + "'"));
-            
-            add(projectid);
-            add(projectname);
-            add(description);
-            add(startdate);
-            add(enddate);
-            add(cost);
-            add(status);
-            add(priority);
-            add(projectmanager);
-            add(country);
-            
-            JButton btnSpara = new JButton("Save my changes");
-            add(btnSpara);
-            btnSpara.addActionListener(e -> {
-                try{
-                    String newProjectid = projectid.getText();
-                    String newProjectname = projectname.getText();
-                    String newDescription = description.getText();
-                    String newStartdate = startdate.getText();
-                    String newEnddate = enddate.getText();
-                    String newCost = cost.getText();
-                    String newStatus = status.getText();
-                    String newPriority = priority.getText();
-                    String newProjectmanager = projectmanager.getText();
-                    String newCountry = country.getText();
-            
-                    idb.update("UPDATE projekt SET projektnamn = '" + newProjectname + 
-                            "', beskrivning = '" + newDescription + 
-                            "', startdatum = '" + newStartdate + 
-                            "', slutdatum = '" + newEnddate + 
-                            "', kostnad = '" + newCost + 
-                            "', status = '" + newStatus + 
-                            "', prioritet = '" + newPriority + 
-                            "', projektchef = '" + newProjectmanager + 
-                            "', land = '" + newCountry + 
-                            "' WHERE projektchef = '" + id + "'");
-                }catch(InfException error){
-                    System.out.println(error);
-                }
-            });
+            JPanel mainPanel = new JPanel();
+            mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+            ArrayList<HashMap<String, String>> allProjects = idb.fetchRows("select * from projekt where projektchef = '" + id + "'");
+
+            for (HashMap<String, String> project : allProjects) {
+                JPanel projectPanel = new JPanel();
+                projectPanel.setLayout(new BoxLayout(projectPanel, BoxLayout.Y_AXIS));
+                final String pID = project.get("pid");
+                projectPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Project: " + project.get("pid")));
+
+                final JTextField projectid = new JTextField(project.get("pid"), 60);
+                final JTextField projectname = new JTextField(project.get("projektnamn"), 60);
+                final JTextField description = new JTextField(project.get("beskrivning"), 60);
+                final JTextField startdate = new JTextField(project.get("startdatum"), 60);
+                final JTextField enddate = new JTextField(project.get("slutdatum"), 60);
+                final JTextField cost = new JTextField(project.get("kostnad"), 60);
+                final JTextField status = new JTextField(project.get("status"), 60);
+                final JTextField priority = new JTextField(project.get("prioritet"), 60);
+                final JTextField projectmanager = new JTextField(project.get("projektchef"), 60);
+                final JTextField country = new JTextField(project.get("land"), 60);
+
+                JButton btnSave = new JButton("Save my changes");
+
+                btnSave.addActionListener(e -> {
+                    try {
+                        String query = "UPDATE projekt SET projektnamn = '" + projectname.getText()
+                                + "', beskrivning = '" + description.getText()
+                                + "', startdatum = '" + startdate.getText()
+                                + "', slutdatum = '" + enddate.getText()
+                                + "', kostnad = '" + cost.getText()
+                                + "', status = '" + status.getText()
+                                + "', prioritet = '" + priority.getText()
+                                + "', projektchef = '" + projectmanager.getText()
+                                + "', land = '" + country.getText()
+                                + "' WHERE pid = '" + pID + "'";
+                        idb.update(query);
+                    } catch (InfException error) {
+                        System.out.println(error);
+                    }
+                });
+                projectid.setEditable(false);
+                projectPanel.add(projectid);
+                projectPanel.add(projectname);
+                projectPanel.add(description);
+                projectPanel.add(startdate);
+                projectPanel.add(enddate);
+                projectPanel.add(cost);
+                projectPanel.add(status);
+                projectPanel.add(priority);
+                projectPanel.add(projectmanager);
+                projectPanel.add(country);
+                projectPanel.add(btnSave);
+
+                mainPanel.add(projectPanel);
+            }
+            JScrollPane scrollPane = new JScrollPane(mainPanel);
+            add(scrollPane);
             revalidate();
             repaint();
-            
-        }catch(InfException e){
+
+        } catch (InfException e) {
             System.out.println(e);
         }
     }
