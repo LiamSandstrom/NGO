1cd3930a0aaab71ac6809924a71c7c3972af4c24
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2025-05-18 13:47:59 +0200

    råkade jobba i main

--- src/NGO/UI/ChangePartnerInfo.java
+++ src/NGO/UI/ChangePartnerInfo.java
@@ -31,28 +31,31 @@ public class ChangePartnerInfo extends ContentPanelStructure {
             setBackground(Color.gray);
             id = user.getId();
             idb = user.getDb();
+            
+            
+            
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-            ArrayList<HashMap<String, String>> allPartners = idb.fetchRows("select * from partner join stad on stad = sid join projekt_partner on partner.pid = projekt_partner.partner_pid join projekt on projekt_partner.pid = projekt.pid where projektchef = '" + id + "'");
+            //ArrayList<HashMap<String, String>> allPartners = idb.fetchRows("select * from partner join stad on stad = sid join projekt_partner on partner.pid = projekt_partner.partner_pid join projekt on projekt_partner.pid = projekt.pid where projektchef = '" + id + "'");
+            ArrayList<HashMap<String, String>> allPartners = idb.fetchRows("select projekt_partner.pid, projektnamn, partner_pid, namn from projekt_partner join partner on partner_pid = partner.pid join projekt on projekt_partner.pid = projekt.pid where projektchef = '" + id + "'");
             for(HashMap<String, String> partners : allPartners){
                 JPanel partnerPanel = new JPanel();
                 partnerPanel.setLayout(new BoxLayout(partnerPanel, BoxLayout.Y_AXIS));
                 String partnerID = partners.get("pid");
                 //partnerPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Project: " + partners.get("projekt.pid")));
                 
-                JTextField partnerid = new JTextField(partners.get("pid"), 60);
-                JTextField partnername = new JTextField(partners.get("namn"), 60);
-                JTextField kontactperson = new JTextField(partners.get("kontaktperson"), 60);
-                JTextField kontactemail = new JTextField(partners.get("kontaktepost"), 60);
-                JTextField phone = new JTextField(partners.get("telefon"), 60);
-                JTextField addres = new JTextField(partners.get("adress"), 60);
-                JTextField branch = new JTextField(partners.get("branch"), 60);
-                //JTextField city = new JTextField(partners.get("stad.namn"), 60);
+                JTextField projectid = new JTextField(partners.get("projekt_partner.pid"), 60);
+                JTextField projectname = new JTextField(partners.get("projektnamn"), 60);
                 
-                JButton btnSave = new JButton("Save my changes");
-                btnSave.addActionListener(e -> {
-                    try{
-                        String query = "update partner set partner.namn = '" + partnername.getText()
+                for(HashMap<String, String> partner : allPartners){
+                    JTextField partnerid = new JTextField(partner.get("partner_pid"), 60);
+                    JTextField partnername = new JTextField(partner.get("namn"), 60);
+                
+                
+                    /*JButton btnSave = new JButton("Save my changes");
+                    btnSave.addActionListener(e -> {
+                        try{
+                            String query = "update partner set partner.namn = '" + partnername.getText()
                                 +"', kontaktperson = '" + kontactperson.getText()
                                 +"', kontaktepost = '" + kontactemail.getText()
                                 +"', telefon = '" + phone.getText()
@@ -60,23 +63,22 @@ public class ChangePartnerInfo extends ContentPanelStructure {
                                 +"', branch = '" + branch.getText()
                                 //+"', stad.namn = '" + city.getText()
                                 +"' where pid = '" + partnerID + "'";
-                        idb.update(query);
-                    }catch(InfException error){
-                        System.out.println(error.getMessage());
-                    }
-                });
-                partnerid.setEditable(false);
-                partnerPanel.add(partnerid);
-                partnerPanel.add(partnername);
-                partnerPanel.add(kontactperson);
-                partnerPanel.add(kontactemail);
-                partnerPanel.add(phone);
-                partnerPanel.add(addres);
-                partnerPanel.add(branch);
-                //partnerPanel.add(city);
-                partnerPanel.add(btnSave);
-                
-                mainPanel.add(partnerPanel);
+                            idb.update(query);
+                        }catch(InfException error){
+                            System.out.println(error.getMessage());
+                        }
+                    });*/
+                    projectid.setEditable(false);
+                    partnerPanel.add(projectid);
+                    projectname.setEditable(false);
+                    partnerPanel.add(projectname);
+                    partnerid.setEditable(false);
+                    partnerPanel.add(partnerid);
+                    partnername.setEditable(false);
+                    partnerPanel.add(partnername);
+                    
+                    mainPanel.add(partnerPanel);
+                }
             }
             JScrollPane scrollPane = new JScrollPane(mainPanel);
             add(scrollPane);
