58ef661653754cc5efd8531a51a70bea2eae11f9
kanatdavid <david.kanat@hotmail.com>
2025-05-19 11:15:36 +0200

    vx

---
+++ src/NGO/UI/AddRemoveHandläggareOnProject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NGO.UI;
+
+import NGO.User;
+import oru.inf.InfDB;
+
+/**
+ *
+ * @author david
+ */
+public class AddRemoveHandläggareOnProject extends UIStructure{
+    private User user;
+    private InfDB idb;
+    
+    public AddRemoveHandläggareOnProject(User user, UIStructure uiStructure){
+        super();
+        this.user = user;
+        
+    }
+}
