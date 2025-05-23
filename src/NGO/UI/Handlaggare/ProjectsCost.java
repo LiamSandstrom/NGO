8e78e8aca4b0ba2f72721b7d055fe55c90d93317
Pontus "HeyHey <pontuskumla@gmail.com>
2025-05-23 15:01:07 +0200

    merge show my cost och show my projects

--- src/NGO/UI/Handlaggare/ProjectsCost.java
+++ src/NGO/UI/Handlaggare/ProjectsCost.java
@@ -21,62 +21,72 @@ import oru.inf.InfException;
  *
  * @author Pontus
  */
-public class ProjectsCost extends ContentPanelStructure{
-    
+public class ProjectsCost extends ContentPanelStructure {
+
     String id;
     InfDB idb;
     User user;
     ScrollListPanel cardList;
     GridBagConstraints gbc;
     double totalCost;
-    
+    ArrayList<String> chefId;
+    boolean kontroll;
+
     public ProjectsCost(User user, UIStructure parentPanel) throws InfException {
         super(user, parentPanel);
         this.user = user;
         id = user.getId();
         idb = user.getDb();
         cardList = null;
-        idb = user.getDb();
+        chefId = new ArrayList<>();
+        chefId = idb.fetchColumn("select projektchef from projekt");
+        
+
         totalCost = 0;
         setLayout(new GridBagLayout());
         gbc = new GridBagConstraints();
-        
-        
-        ArrayList<String> sqlQuery = idb.fetchColumn("select kostnad from projekt where projektchef = '" + id + "'");
-        for(String cost : sqlQuery){
-            double tot = Double.parseDouble(cost);
-            totalCost += tot;
-        }
-        JLabel totalCostLabel = new JLabel("Total kostnad: " + totalCost + " kr");
-        totalCostLabel.setFont(new Font("Arial", Font.PLAIN, 42));
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.anchor = GridBagConstraints.CENTER;
-        add(totalCostLabel, gbc);
 
-        
-        gbc.gridy = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        
-        
-        createList("select * from projekt where projektchef = " + id + ";");
+        for (String ettID : chefId) {
+            if (ettID.equals(id)) {
+                kontroll = true;
+            }
+        }
+        if (kontroll) {
+
+            ArrayList<String> sqlQuery = idb.fetchColumn("select kostnad from projekt where projektchef = '" + id + "'");
+            for (String cost : sqlQuery) {
+                double tot = Double.parseDouble(cost);
+                totalCost += tot;
+            }
+            JLabel totalCostLabel = new JLabel("Total kostnad: " + totalCost + " kr");
+            totalCostLabel.setFont(new Font("Arial", Font.PLAIN, 42));
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.gridwidth = 1;
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.anchor = GridBagConstraints.CENTER;
+            add(totalCostLabel, gbc);
+
+            gbc.gridy = 1;
+            gbc.fill = GridBagConstraints.BOTH;
+            createList("select * from projekt where projektchef = " + id + ";");
+        }
+        createList("select * from projekt where pid in (select pid from ans_proj where aid in (select aid from anstalld where avdelning = (select avdelning from anstalld where aid = '" + id + "')))");
     }
-    
+
     public void createList(String query) {
-        if(cardList!=null){
+        if (cardList != null) {
             remove(cardList);
             cardList = null;
         }
         try {
-            
+
             ArrayList<String> cost = idb.fetchColumn(query);
 
-             cardList = new ScrollListPanel(
+            cardList = new ScrollListPanel(
                     cost,
                     () -> new CostCard(20, user));
-            add(cardList,gbc);
+            add(cardList, gbc);
             revalidate();
             repaint();
         } catch (Exception e) {
