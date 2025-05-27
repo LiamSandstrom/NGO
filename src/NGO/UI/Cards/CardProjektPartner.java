1e2d1809947f57107ed95b14793d5e1d5788cbff
Rostyslav Malanchuk <rostyslavmalanchuk15@gmail.com>
2025-05-27 17:13:36 +0200

    försök 1000 för att byta till cardstrcture

---
+++ src/NGO/UI/Cards/CardProjektPartner.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NGO.UI.Cards;
+
+import NGO.UI.CardStructure;
+import NGO.UI.PartnerProjektUI;
+import NGO.UI.ProjektPartnerUI;
+import NGO.UI.SettingsJFrameHandler;
+import NGO.User;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author rostykmalanchuk
+ */
+public class CardProjektPartner extends CardStructure{
+    
+    private User user;
+    private InfDB idb;
+    private String id;
+
+    public CardProjektPartner(int radius, User user) {
+        super(radius, user);
+        this.user = user;
+        idb = user.getDb();
+        id = user.getId();
+        
+    }
+
+    @Override
+    public void initCard(String id) {
+        try{
+            String projName = idb.fetchSingle("select projektnamn from projekt where projektchef = '" + id + "'");
+            
+            JLabel cardRbr = new JLabel(projName);
+            cardRbr.setFont(new Font("Arial", Font.PLAIN, 20));
+            add(cardRbr, BorderLayout.CENTER);
+
+            JButton infoBtn = new JButton("Partners");
+            infoBtn.setPreferredSize(new Dimension(100, 33));
+            infoBtn.setFont(new Font("Arial", Font.PLAIN, 16));
+            infoBtn.setBackground(new Color(63, 81, 181));
+            add(infoBtn, BorderLayout.EAST);
+
+            infoBtn.addActionListener(e -> {
+                SettingsJFrameHandler.addPanel(new PartnerProjektUI(user, id));
+            });
+        }catch(InfException e){
+            JOptionPane.showMessageDialog(this, e);
+        }
+        
+    }
+    
+}
---
+++ src/NGO/UI/PartnerProjektUI.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NGO.UI;
+
+import NGO.User;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author rostykmalanchuk
+ */
+public class PartnerProjektUI extends SettingsPanelFramework{
+    private InfDB idb;
+    private User user;
+    private String id;
+    
+    public PartnerProjektUI(User user, String id) {
+        super(user, id);
+        this.user = user;
+        this.idb = user.getDb();
+        this.id = id;
+        
+    }
+    
+    private void getProjects(String userid){
+        try{
+            ArrayList<String> projList = idb.fetchColumn("select pid from projekt where projektchef = '" + userid + "'");
+            for(String pid : projList){
+                addInfo(" ", pid);
+                
+                HashMap<String, String> partners = idb.fetchRow("select partner_pid, namn from projekt_partner join partner on partner_pid = partner.pid join projekt on projekt.pid = projekt_partner.pid where projekt_partner.pid = '" + pid + "'");
+                
+            }
+            
+            
+            setInfo();
+        }catch(InfException e){
+            JOptionPane.showMessageDialog(this, e);
+        }
+        
+        
+    }
+    
+}
